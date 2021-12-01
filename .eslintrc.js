715f2f4f60e10fc69d9721398756282c46e64775
JunHyeongB <aowlr0513@gmail.com>
2021-12-02 02:45:45 +0900

    style: eslint airbnb에서 recommended로 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'airbnb',
     'plugin:react/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:import/errors',
@@ -23,34 +22,7 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
-    'linebreak-style': 0,
-    'import/prefer-default-export': 0,
-    'import/extensions': 0,
-    'no-use-before-define': 0,
-    'import/no-unresolved': 0,
     'react/react-in-jsx-scope': 0,
-    'import/no-extraneous-dependencies': 0,
-    'no-shadow': 0,
-    'react/prop-types': 0,
-    'react/jsx-one-expression-per-line': 0,
-    'func-names': ['error', 'as-needed'],
-    'react/jsx-filename-extension': [
-      2,
-      {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    ],
-    'object-curly-newline': [
-      'error',
-      {
-        ExportDeclaration: {
-          multiline: true,
-          minProperties: 2,
-        },
-      },
-    ],
-    'jsx-a11y/no-noninteractive-element-interactions': 0,
-    '@typescript-eslint/explicit-module-boundary-types': 0,
   },
   settings: {
     'import/resolver': {
