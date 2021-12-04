3e0440a39282c92b7849d3fd568e3b3863373e7b
dasssseul <daseul8086@gmail.com>
2021-12-05 02:34:04 +0900

    fix: index 파일 충돌 해결

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,3 @@
 export { Container } from './atoms/Container';
-<<<<<<< HEAD
 export { Text } from './atoms/Text';
-=======
 export { Input } from './atoms/Input';
->>>>>>> f410287da2fbddd1d23e43213641de2310dda6e1
