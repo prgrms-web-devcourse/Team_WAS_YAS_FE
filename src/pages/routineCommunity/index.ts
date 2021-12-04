0123febb6239bc8351774dde6c2edcec3b1b213b
younoah <kr_uno_10@naver.com>
2021-12-05 00:32:08 +0900

    feat: 라우터 컴포넌트 구현

--- src/pages/routineCommunity/index.ts
+++ src/pages/routineCommunity/index.ts
@@ -1,3 +1,3 @@
 export { default as RoutineCommunityPage } from './RoutineCommunityPage';
-export { default as RoutinePostPage } from './RoutinePostPage';
+export { default as RoutinePostCreatePage } from './RoutinePostCreatePage';
 export { default as RoutinePostDetailPage } from './RoutinePostDetailPage';
