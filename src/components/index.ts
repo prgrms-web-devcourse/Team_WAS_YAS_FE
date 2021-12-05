69871626363a08ff10f608ed185ae68ed3a6ebb3
JunHyeongB <aowlr0513@gmail.com>
2021-12-05 16:29:01 +0900

    style: RoundedButtonProps export 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { Container } from './atoms/Container';
 export { Text } from './atoms/Text';
 export { Input } from './atoms/Input';
 export { RoundedButton } from './atoms/RoundedButton';
+export type { RoundedButtonProps } from './atoms/RoundedButton/Base';
