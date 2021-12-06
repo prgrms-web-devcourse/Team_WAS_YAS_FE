13cac74049209ca37f17b549d5d5f66fd24f16e3
younoah <kr_uno_10@naver.com>
2021-12-07 01:57:08 +0900

    refactor: 나의루틴 페이지 복구

--- src/pages/myRoutine/MyRoutinePage.tsx
+++ src/pages/myRoutine/MyRoutinePage.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import { Container } from '@/components';
-import { Header, IconButton } from '@/components';
 
 const MyRoutinePage = (): JSX.Element => {
   return (
     <Container>
-      <Header>
-        <IconButton.Back />
-        <IconButton.UserProfile />
-      </Header>
-      {/* <h1>MyRoutinePage</h1> */}
+      <h1>MyRoutinePage</h1>{' '}
     </Container>
   );
 };
