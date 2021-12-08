c062cea319603ff53fcb374ca77879e637fb5dc9
younoah <kr_uno_10@naver.com>
2021-12-09 01:47:33 +0900

    feat: apis에 선언되어있는  모델 타입을 models로 이동

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,8 +1,9 @@
-export type { UserType } from './types';
-export type { RoutineType } from './types';
-export type { MissionType } from './types';
-export type { RoutineCompletionType } from './types';
-export type { RoutineInfoType } from './types';
-export type { MissionCompletionType } from './types';
-export type { RoutinePostType } from './types';
-export type { CommentType } from './types';
+export {};
+// export type { UserType } from '../models/types';
+// export type { RoutineType } from '../models/types';
+// export type { MissionType } from '../models/types';
+// export type { RoutineCompletionType } from '../models/types';
+// export type { RoutineInfoType } from '../models/types';
+// export type { MissionCompletionType } from '../models/types';
+// export type { RoutinePostType } from '../models/types';
+// export type { CommentType } from '../models/types';
