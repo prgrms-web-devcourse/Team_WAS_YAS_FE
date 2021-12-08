cc2a57c26707b39fc6ff39af04eef72b681ed6d7
younoah <kr_uno_10@naver.com>
2021-12-08 15:05:43 +0900

    feat: 스토리북 필요없는 코드 정리

--- src/stories/components/atoms/LikeBox.stories.tsx
+++ src/stories/components/atoms/LikeBox.stories.tsx
@@ -5,20 +5,11 @@ export default {
   component: LikeBox,
   argTypes: {
     active: { control: 'boolean' },
+    count: { control: 'number' },
+    interactive: { control: 'boolean' },
   },
 };
 
 export const Default = ({ ...args }: LikeBoxProps): JSX.Element => {
-  const handleClick = () => {
-    console.log('토글!');
-  };
-
-  return (
-    <>
-      <LikeBox onClick={handleClick} active />
-      <LikeBox onClick={handleClick} count={32} />
-      <LikeBox onClick={handleClick} count={12} active interactive />
-      <LikeBox onClick={handleClick} count={32} interactive />
-    </>
-  );
+  return <LikeBox {...args} />;
 };
