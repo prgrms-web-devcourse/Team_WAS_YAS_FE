6c7971392fca135bc239e81851baed0985a377a3
younoah <kr_uno_10@naver.com>
2021-12-09 16:13:39 +0900

    feat: normalizr 패키지 추가

--- src/store/routinePosts.js
+++ src/store/routinePosts.js
@@ -1,3 +1,4 @@
+const GET_ROUTINE_POSTS = 'routine/GET_ROUTINE_POSTS';
 const CREATE_ROUTINE_POSTS = 'routine/CREATE_ROUTINE_POSTS';
 const UPDATE_ROUTINE_POSTS = 'routine/UPDATE_ROUTINE_POSTS';
 const DELETE_ROUTINE_POSTS = 'routine/DELETE_ROUTINE_POSTS';
@@ -7,6 +8,10 @@ const DELETE_ROUTINE_POSTS = 'routine/DELETE_ROUTINE_POSTS';
 //   routinePosts: {};
 // }
 
+export const getRoutine = () => ({
+  type: GET_ROUTINE_POSTS,
+  routinePosts: {},
+});
 export const createRoutine = () => ({
   type: CREATE_ROUTINE_POSTS,
   routinePosts: {},
@@ -24,6 +29,8 @@ const initialState = [{}];
 
 const routinePosts = (state = initialState, action) => {
   switch (action.type) {
+    case CREATE_ROUTINE_POSTS:
+      return state;
     case CREATE_ROUTINE_POSTS:
       return state;
     case UPDATE_ROUTINE_POSTS:
