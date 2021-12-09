3f7aa2e47ee5646f639dd11cee3e00ec094bcbbc
JunHyeongB <aowlr0513@gmail.com>
2021-12-10 01:12:56 +0900

    feat: RoutineDetailPage 스토리북 추가

---
+++ src/stories/pages/RoutineDetailPage.stories.tsx
@@ -0,0 +1,14 @@
+import { RoutineDetailPage } from '@/pages';
+
+export default {
+  title: 'Components/Pages/RoutineDetailPage',
+  component: RoutineDetailPage,
+};
+
+export const Default = (): JSX.Element => {
+  return (
+    <>
+      <RoutineDetailPage />
+    </>
+  );
+};
