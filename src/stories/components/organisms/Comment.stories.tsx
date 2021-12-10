a123afa6ab9c0986979c84245e793d4664f6825a
younoah <kr_uno_10@naver.com>
2021-12-11 01:07:23 +0900

    feat: 스토리북 수정

--- src/stories/components/organisms/Comment.stories.tsx
+++ src/stories/components/organisms/Comment.stories.tsx
@@ -1,10 +1,28 @@
 import { Comment, CommentProps } from '@/components';
+import { UserType, CommentType } from '@/Models';
 
 export default {
   title: 'Component/Organisms/Comment',
   component: Comment,
 };
 
-export const Default = (): JSX.Element => {
+const user: UserType = {
+  userId: 123,
+  userName: '작성자',
+  nickName: '노아',
+  profileImageUrl: 'https://picsum.photos/200',
+  email: 'yas@yas.com',
+};
+
+const comment: CommentType = {
+  commentId: 321,
+  text: '노아가 댓글을 작성했습니다.',
+  userId: '123ㄴ',
+  createdAt: '2021-01-01 11:11',
+  updatedAt: '2021-01-01 11:11',
+  likes: [],
+};
+
+export const Default = ({ ...args }: CommentProps): JSX.Element => {
   return <></>;
 };
