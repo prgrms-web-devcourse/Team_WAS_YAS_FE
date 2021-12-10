0e3845f96fdfedfcd73b636f0b06a33798d8dd8b
younoah <kr_uno_10@naver.com>
2021-12-11 01:16:23 +0900

    feat: 스토리북 구성

--- src/stories/components/organisms/Comment.stories.tsx
+++ src/stories/components/organisms/Comment.stories.tsx
@@ -1,12 +1,13 @@
 import { Comment, CommentProps } from '@/components';
 import { UserType, CommentType } from '@/Models';
+import styled from '@emotion/styled';
 
 export default {
-  title: 'Component/Organisms/Comment',
+  title: 'Components/Organisms/Comment',
   component: Comment,
 };
 
-const user: UserType = {
+const user1: UserType = {
   userId: 123,
   userName: '작성자',
   nickName: '노아',
@@ -14,15 +15,41 @@ const user: UserType = {
   email: 'yas@yas.com',
 };
 
-const comment: CommentType = {
+const user2: UserType = {
+  userId: 123,
+  userName: '작성자',
+  nickName: '죠지',
+  profileImageUrl: 'https://picsum.photos/300',
+  email: 'yas@yas.com',
+};
+
+const comment1: CommentType = {
   commentId: 321,
   text: '노아가 댓글을 작성했습니다.',
-  userId: '123ㄴ',
+  userId: 123,
   createdAt: '2021-01-01 11:11',
   updatedAt: '2021-01-01 11:11',
   likes: [],
 };
 
+const comment2: CommentType = {
+  commentId: 321,
+  text: '죠지가 조이를 쳐다봅니다.',
+  userId: 123,
+  createdAt: '2021-01-01 11:11',
+  updatedAt: '2021-01-01 11:11',
+  likes: [],
+};
+
+const StyledComment = styled(Comment)`
+  margin-top: 4rem;
+`;
+
 export const Default = ({ ...args }: CommentProps): JSX.Element => {
-  return <></>;
+  return (
+    <>
+      <StyledComment user={user1} comment={comment1} editable></StyledComment>
+      <StyledComment user={user2} comment={comment2}></StyledComment>
+    </>
+  );
 };
