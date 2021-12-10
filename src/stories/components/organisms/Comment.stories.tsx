f953253f71f7f78b154612623e89622eabe3bc0f
younoah <kr_uno_10@naver.com>
2021-12-11 01:01:54 +0900

    feat: 스토리북 생성

---
+++ src/stories/components/organisms/Comment.stories.tsx
@@ -0,0 +1,10 @@
+import { Comment, CommentProps } from '@/components';
+
+export default {
+  title: 'Component/Organisms/Comment',
+  component: Comment,
+};
+
+export const Default = (): JSX.Element => {
+  return <></>;
+};
