fe6ab0b40e162270770b53f040013a47c0761b05
younoah <kr_uno_10@naver.com>
2021-12-13 17:48:03 +0900

    feat: 루틴 더미 데이터 생성

--- src/Models/dummy.ts
+++ src/Models/dummy.ts
@@ -1,4 +1,4 @@
-import { UserType } from '@/Models';
+import { UserType, RoutineType } from '@/Models';
 
 export const userDummy: UserType = {
   userId: 123,
@@ -6,4 +6,16 @@ export const userDummy: UserType = {
   nickName: '아이엠보이',
   profileImageUrl: 'https://picsum.photos/200',
   email: 'yas@yas.com',
+};
+
+export const routineDummy: RoutineType = {
+  routineId: 123,
+  title: '집 앞 공원 산책하기',
+  emoji: '🌳',
+  color: '#66CE92',
+  startGoalTime: `${new Date(2021, 12, 24, 12, 0).toISOString()}`,
+  durationGoalTime: 1000,
+  weeks: ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'],
+  routineCategories: ['HEALTH', 'EXERCISE'],
+  missions: [],
 };
