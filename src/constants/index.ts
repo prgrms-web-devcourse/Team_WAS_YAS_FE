151e50e1608eac07ef5a707fba8f88505bb4b4c2
younoah <kr_uno_10@naver.com>
2021-12-14 18:24:25 +0900

    refactor: 요일 상수명 수정

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -16,11 +16,11 @@ export const ROUTINE_CATEGORY = Object.freeze({
 });
 
 export const WEEK = Object.freeze({
-  MONDAY: '월',
-  TUESDAY: '화',
-  WEDNESDAY: '수',
-  THURSDAY: '목',
-  FRIDAY: '금',
-  SATURDAY: '토',
-  SUNDAY: '일',
+  MON: '월',
+  TUE: '화',
+  WED: '수',
+  THU: '목',
+  FRI: '금',
+  SAT: '토',
+  SUN: '일',
 });
