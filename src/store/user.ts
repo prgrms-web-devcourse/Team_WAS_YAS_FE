d23ab39f2e6377200718a37559e3e9a879e09ea0
younoah <kr_uno_10@naver.com>
2021-12-16 01:05:27 +0900

    feat: 유저 리듀서명 변경

--- src/store/user.ts
+++ src/store/user.ts
@@ -7,11 +7,8 @@ interface UserActionType {
 
 const SET_USER = 'user/SET_USER';
 
-/* 액션 생성함수 만들기 */
-// 액션 생성함수를 만들고 export 키워드를 사용해서 내보내주세요.
 export const setUser = (user: UserType) => ({ type: SET_USER, user });
 
-/* 초기 상태 선언 */
 const initialState: UserType = {
   userId: 0,
   name: '',
@@ -20,9 +17,7 @@ const initialState: UserType = {
   email: '',
 };
 
-/* 리듀서 선언 */
-// 리듀서는 export default 로 내보내주세요.
-export default function counter(state = initialState, action: UserActionType) {
+export default function user(state = initialState, action: UserActionType) {
   switch (action.type) {
     case SET_USER:
       return action.user;
