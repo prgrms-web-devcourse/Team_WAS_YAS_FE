4a04c86b5be95dc1d5068f966e798b8e7bf63468
dasssseul <daseul8086@gmail.com>
2021-12-15 13:05:05 +0900

    feat: Analysis Page 컴포넌트 구현

---
+++ src/stories/pages/AnalysisPage.stories.tsx
@@ -0,0 +1,10 @@
+import { AnalysisPage } from '@/pages';
+
+export default {
+  title: 'Components/Pages/AnalysisPage',
+  component: AnalysisPage,
+};
+
+export const Default = (): JSX.Element => {
+  return <AnalysisPage />;
+};
