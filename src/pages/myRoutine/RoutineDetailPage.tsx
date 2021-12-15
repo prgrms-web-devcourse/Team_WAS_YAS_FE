4fc7fda7e9f8235529b5675c6237c90e4850d78e
JunHyeongB <aowlr0513@gmail.com>
2021-12-15 23:37:22 +0900

    feat: RoutineDetailPage Drag & Drop 기능 추가

--- src/pages/myRoutine/RoutineDetailPage.tsx
+++ src/pages/myRoutine/RoutineDetailPage.tsx
@@ -1,8 +1,8 @@
 import {
   Button,
   Container,
+  DraggableMission,
   IconButton,
-  Mission,
   RoundedButton,
   RoutineCategory,
   RoutineInfo,
@@ -10,7 +10,11 @@ import {
 import { RoutineType, MissionType } from '@/Models';
 import { Colors, Media } from '@/styles';
 import styled from '@emotion/styled';
+import { useCallback, useState } from 'react';
 import { Link } from 'react-router-dom';
+import update from 'immutability-helper';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 interface ExtendedMissionType extends MissionType {
   userDurationTime?: number;
@@ -28,7 +32,10 @@ const DUMMY_ROUTINE_DETAIL: Partial<ExtendedRoutineType> = {
   durationGoalTime: 14200,
   startGoalTime: `${new Date().toISOString()}`,
   routineCategories: ['운동', '공부'],
-  missions: [
+};
+
+const RoutineDetailPage = (): JSX.Element => {
+  const [missions, setMissions] = useState([
     {
       missionId: 1,
       emoji: '🌳',
@@ -93,46 +100,62 @@ const DUMMY_ROUTINE_DETAIL: Partial<ExtendedRoutineType> = {
       title: '공부하기',
       durationGoalTime: 1800,
     },
-  ],
-};
+  ]);
+  const moveMission = useCallback(
+    (dragIndex: number, hoverIndex: number) => {
+      const dragMission = missions[dragIndex];
+      setMissions(
+        update(missions, {
+          $splice: [
+            [dragIndex, 1],
+            [hoverIndex, 0, dragMission],
+          ],
+        }),
+      );
+    },
+    [missions],
+  );
 
-const RoutineDetailPage = (): JSX.Element => {
   return (
-    <Container>
-      <RoutineInfo routineObject={DUMMY_ROUTINE_DETAIL} />
-      <CategoryEditFlexBox>
-        <div>
-          {DUMMY_ROUTINE_DETAIL.routineCategories?.map((category, i) => (
-            <StyledCategory key={i}>{category}</StyledCategory>
-          ))}
-        </div>
-        <RoundedButton.Edit />
-      </CategoryEditFlexBox>
-      {DUMMY_ROUTINE_DETAIL.missions?.map((mission) => (
-        <StyledMission
-          type="normal"
-          key={mission.missionId}
-          missionObject={mission}
-        />
-      ))}
-      <Link to={`/routine/${DUMMY_ROUTINE_DETAIL.routineId}/progress`}>
-        <StyledButton colorType="white">
-          <Svg
-            viewBox="0 0 24 28"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M22.7869 15.5913L3.69882 26.6667C2.0789 27.6057 0 26.4687 0 24.5758V2.42492C0 0.535018 2.0759 -0.604927 3.69882 0.337027L22.7869 11.4125C23.1554 11.6228 23.4617 11.9269 23.6747 12.2939C23.8878 12.6608 24 13.0776 24 13.5019C24 13.9262 23.8878 14.343 23.6747 14.7099C23.4617 15.0768 23.1554 15.3809 22.7869 15.5913Z"
-              fill="#565656"
-            />
-          </Svg>
-        </StyledButton>
-      </Link>
-      <Link to={`/routine/${DUMMY_ROUTINE_DETAIL.routineId}/create`}>
-        <StyledRoutineAddButton />
-      </Link>
-    </Container>
+    <DndProvider backend={HTML5Backend}>
+      <Container>
+        <RoutineInfo routineObject={DUMMY_ROUTINE_DETAIL} />
+        <CategoryEditFlexBox>
+          <div>
+            {DUMMY_ROUTINE_DETAIL.routineCategories?.map((category, i) => (
+              <StyledCategory key={i}>{category}</StyledCategory>
+            ))}
+          </div>
+          <RoundedButton.Edit />
+        </CategoryEditFlexBox>
+        {missions.map((mission, index) => (
+          <StyledMission
+            moveMission={moveMission}
+            index={index}
+            type="normal"
+            key={mission.missionId}
+            missionObject={mission}
+          />
+        ))}
+        <Link to={`/routine/${DUMMY_ROUTINE_DETAIL.routineId}/progress`}>
+          <StyledButton colorType="white">
+            <Svg
+              viewBox="0 0 24 28"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M22.7869 15.5913L3.69882 26.6667C2.0789 27.6057 0 26.4687 0 24.5758V2.42492C0 0.535018 2.0759 -0.604927 3.69882 0.337027L22.7869 11.4125C23.1554 11.6228 23.4617 11.9269 23.6747 12.2939C23.8878 12.6608 24 13.0776 24 13.5019C24 13.9262 23.8878 14.343 23.6747 14.7099C23.4617 15.0768 23.1554 15.3809 22.7869 15.5913Z"
+                fill="#565656"
+              />
+            </Svg>
+          </StyledButton>
+        </Link>
+        <Link to={`/routine/${DUMMY_ROUTINE_DETAIL.routineId}/create`}>
+          <StyledRoutineAddButton />
+        </Link>
+      </Container>
+    </DndProvider>
   );
 };
 
@@ -158,7 +181,7 @@ const StyledCategory = styled(RoutineCategory)`
   }
 `;
 
-const StyledMission = styled(Mission)`
+const StyledMission = styled(DraggableMission)`
   margin-bottom: 1rem;
 `;
 
