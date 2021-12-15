ac3e6c1fe50a186c9dd4bba2476ad866c91f8332
dasssseul <daseul8086@gmail.com>
2021-12-15 13:05:32 +0900

    refactor: notFoundPage 스토리북 추가

---
+++ src/stories/pages/NotFoundPage.stories.tsx
@@ -0,0 +1,10 @@
+import { NotFoundPage } from '@/pages';
+
+export default {
+  title: 'Components/Pages/NotFoundPage',
+  component: NotFoundPage,
+};
+
+export const Default = (): JSX.Element => {
+  return <NotFoundPage />;
+};
