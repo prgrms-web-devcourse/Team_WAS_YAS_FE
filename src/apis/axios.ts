e703033cfb6920c95a80e5d9b3678d81debf06de
younoah <kr_uno_10@naver.com>
2021-12-15 13:40:47 +0900

    feat: auth요청과 일반 요청 인스턴스 세팅 함수 분리

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -8,25 +8,49 @@ import {
 
 const API_END_POINT = ' http://52.79.219.24:8080';
 
-const setInterceptors = (instance: AxiosInstance, auth?: boolean) => {
-  auth &&
-    instance.interceptors.request.use(
-      (config) => {
-        const rawToken = sessionStorage.getItem('YAS_USER_TOKEN');
-        if (!rawToken) return;
-        const TOKEN = JSON.parse(rawToken);
-        // config.headers.Authorization = `bearer ${TOKEN}`;
-        config.headers = {
-          ...config.headers,
-          token: TOKEN,
-          'Content-Type': 'application/json',
-        };
-        return config;
-      },
-      (error) => {
-        return Promise.reject(error.response);
-      },
-    );
+const setInterceptors = (instance: AxiosInstance) => {
+  instance.interceptors.request.use(
+    (config) => {
+      config.headers = {
+        ...config.headers,
+        'Content-Type': 'application/json',
+      };
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error.response);
+    },
+  );
+
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      return Promise.reject(error.response);
+    },
+  );
+  return instance;
+};
+
+const setAuthInterceptors = (instance: AxiosInstance) => {
+  instance.interceptors.request.use(
+    (config) => {
+      const rawToken = sessionStorage.getItem('YAS_USER_TOKEN');
+      if (!rawToken) return;
+      const TOKEN = JSON.parse(rawToken);
+      // config.headers.Authorization = `bearer ${TOKEN}`;
+      config.headers = {
+        ...config.headers,
+        token: TOKEN,
+        'Content-Type': 'application/json',
+      };
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error.response);
+    },
+  );
 
   instance.interceptors.response.use(
     (response) => {
@@ -40,13 +64,13 @@ const setInterceptors = (instance: AxiosInstance, auth?: boolean) => {
 };
 
 const createInstance = () => {
-  const instance = axios.create({ baseURL: API_END_POINT });
+  const instance = axios.create({ baseURL: API_END_POINT, timeout: 5000 });
   return setInterceptors(instance);
 };
 export const request = createInstance();
 
 const createInstanceWithAuth = () => {
-  const instance = axios.create({ baseURL: API_END_POINT });
-  return setInterceptors(instance, true);
+  const instance = axios.create({ baseURL: API_END_POINT, timeout: 5000 });
+  return setAuthInterceptors(instance);
 };
 export const authRequest = createInstanceWithAuth();
