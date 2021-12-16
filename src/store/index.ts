2199e9ef41d9d75ea0b225770418c9ba1e179a36
younoah <kr_uno_10@naver.com>
2021-12-16 20:11:31 +0900

    feat: 루틴 스토어 구현

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,16 +5,18 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
 import { user } from './user';
-
-const combinedReducer = combineReducers({
-  user: user.reducer,
-});
+import { routine } from './routine';
 
 const persistConfig = {
   key: 'root',
   storage: session,
-  whitelist: ['user'],
+  whitelist: ['user', 'routine'],
 };
+
+const combinedReducer = combineReducers({
+  user: user.reducer,
+  routine: routine.reducer,
+});
 
 const rootReducer = persistReducer(persistConfig, combinedReducer);
 
