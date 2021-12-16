98914bbfdfdd93559548399016c8ae34b9c4e9dc
younoah <kr_uno_10@naver.com>
2021-12-16 23:37:48 +0900

    feat: TODO 주석 추가

--- src/routes/uitils.ts
+++ src/routes/uitils.ts
@@ -1,5 +1,6 @@
 import { userApi } from '@/apis';
 
+// TODO: 토큰 유효성 검사 api로 대체하기
 export const isLogin = async () => {
   const token = sessionStorage.getItem('YAS_USER_TOKEN');
   if (!token) return false;
