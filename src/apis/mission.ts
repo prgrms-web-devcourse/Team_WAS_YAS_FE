57bf0142cb5074502ec8bc95a17ce2387361ca3a
dasssseul <daseul8086@gmail.com>
2021-12-16 17:28:48 +0900

    feat: mission API 기능 구현

--- src/apis/mission.ts
+++ src/apis/mission.ts
@@ -1,5 +1,36 @@
 // import { request, authRequest } from './config';
 
-const missionApi = {};
+import { AxiosResponse } from 'axios';
+import { authRequest } from './config';
+
+interface MissionApiType {
+  createMission: (
+    routineId: number,
+    missionInfo: {
+      name: string;
+      color: string;
+      emoji: string;
+      durationGoalTime: number;
+      orders: number;
+    },
+  ) => Promise<AxiosResponse>;
+  updateMission: (
+    routineId: number,
+    missionInfo: {
+      missionId: number;
+      orders: number;
+    },
+  ) => Promise<AxiosResponse>;
+  deleteMission: (missionId: number) => Promise<AxiosResponse>;
+}
+
+const missionApi: MissionApiType = {
+  createMission: (routineId, missionInfo) =>
+    authRequest.post(`/routines/${routineId}/missions`, missionInfo),
+  updateMission: (routineId, missionInfo) =>
+    authRequest.put(`/routines/${routineId}/missions`, missionInfo),
+  deleteMission: (missionId) =>
+    authRequest.delete(`/routines/missions/${missionId}`),
+};
 
 export default missionApi;
