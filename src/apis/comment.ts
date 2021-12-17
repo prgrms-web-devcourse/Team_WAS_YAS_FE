e48208cee0ab9754f674c2c15cf8cda3e4618bc7
younoah <kr_uno_10@naver.com>
2021-12-17 16:03:07 +0900

    refactor: post api 모듈 수정

--- src/apis/comment.ts
+++ src/apis/comment.ts
@@ -2,15 +2,18 @@ import { AxiosResponse } from 'axios';
 import { authRequest } from './config';
 
 interface commentApiType {
-  createComment: (postId: number) => Promise<AxiosResponse>;
+  createComment: (postId: number, content: string) => Promise<AxiosResponse>;
   updateComment: (commentId: number) => Promise<AxiosResponse>;
   deleteComment: (commentId: number) => Promise<AxiosResponse>;
 }
 
 const commentApi: commentApiType = {
-  createComment: (postId: number) => authRequest(`/posts/${postId}/comments`),
-  updateComment: (commentId: number) => authRequest(`/comments/${commentId}`),
-  deleteComment: (commentId: number) => authRequest(`/comments/${commentId}`),
+  createComment: (postId: number, content) =>
+    authRequest.post(`/posts/${postId}/comments`, { content }),
+  updateComment: (commentId: number) =>
+    authRequest.patch(`/comments/${commentId}`),
+  deleteComment: (commentId: number) =>
+    authRequest.delete(`/comments/${commentId}`),
 };
 
 export default commentApi;
