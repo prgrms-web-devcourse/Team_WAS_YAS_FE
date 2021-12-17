b7d38b97b8a78ef4ec81ef2d503bccf04c928cf3
younoah <kr_uno_10@naver.com>
2021-12-17 23:13:06 +0900

    feat: 헤더컴포넌트에서 뒤로가기 버튼 컴포넌트 리팩토링

--- src/components/templates/Header/Header.tsx
+++ src/components/templates/Header/Header.tsx
@@ -1,10 +1,10 @@
 import styled from '@emotion/styled';
 import { useRouteMatch, useHistory } from 'react-router-dom';
-import { IconButton, IconButtonProps } from '@/components';
 import { Avatar } from '@mui/material';
 import { Media, Colors } from '@/styles';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/store';
+import BackButton from './BackButton';
 
 export type HeaderProps = React.ComponentProps<'header'>;
 
@@ -13,10 +13,17 @@ const Header = ({ ...props }: HeaderProps): JSX.Element => {
   const [match, history] = [useRouteMatch(), useHistory()];
   const params = parseParams(match.url);
 
+  const handleClickBackButton = () => {
+    history.goBack();
+  };
+
   return (
     <Container {...props}>
       <ContentContainer>
-        <BackButton visible={params.length > 1 && history.length > 1} />
+        <BackButton
+          onClick={handleClickBackButton}
+          visible={params.length > 1 && history.length > 1}
+        />
         <StyledAvatar
           src={user?.profileImage}
           on={params[0] === 'mypage' ? 1 : 0}
@@ -59,12 +66,6 @@ const ContentContainer = styled.div`
   width: 100%;
   height: 80px;
   margin: 0 auto;
-`;
-
-const BackButton = styled(IconButton.Back)<
-  IconButtonProps & { visible: boolean }
->`
-  visibility: ${({ visible }) => (visible ? 'visible' : 'hidden')};
 `;
 
 const StyledAvatar = styled(Avatar)<{ on: number }>`
