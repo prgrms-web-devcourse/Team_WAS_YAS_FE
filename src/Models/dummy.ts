0235fec35db279eef517956bc788fd6f2efc1d38
younoah <kr_uno_10@naver.com>
2021-12-17 16:35:47 +0900

    refactor: 댓글 관련 더미데이터 제거

--- src/Models/dummy.ts
+++ src/Models/dummy.ts
@@ -1,10 +1,4 @@
-import {
-  UserType,
-  RoutineType,
-  RoutinePostType,
-  MissionType,
-  CommentType,
-} from '@/Models';
+import { UserType, RoutineType, MissionType } from '@/Models';
 
 export const userDummy: UserType = {
   userId: 123,
@@ -34,24 +28,3 @@ export const missionDummy: MissionType = {
   durationGoalTime: 3600,
   orders: 0,
 };
-
-// export const routinePostDummy: RoutinePostType = {
-//   routinePostId: 333,
-//   title: '집 앞 공원 산책하기',
-//   userId: 123,
-//   routineId: 321,
-//   createdAt: '2022-12-24T03:00:00.000Z',
-//   updatedAt: '2022-12-24T03:00:00.000Z',
-//   comments: [],
-//   likes: [],
-// };
-
-// export const commentDummy: CommentType = {
-//   commentId: 777,
-//   content:
-//     '노아가 댓글을 작성했습니다.ㅁㅇㄴㄹㅁㅇㅁㅇㄹㄴㅁㅇㄴㄹㅁㄴㅇㄹㄴㄹㅁㄴㅇㄹㅁㅇㄴㅇㄹㅁㄴㅇㄹㅇㄴㅁㄹㅁㅇㄹㄴㅇㅁㄴㄹㅁㄴㅇㄹㅁㄴㅇ\n\n\n\nㅇㄹㅁㄴㅇ\n\n\nㅇㄹㅁㄴㅇ\n\n\nㅇㄹㅁㄴㅇ\n\n\nㅇㄹㅁㄴㅇ\n\n\nㅇㄹㅁㄴㅇ\n\n\nㅇㄹㅁㄴㅇ\n\n\nㅇㄹㅁㄴㅇ\n\n\nㅇㄹㅁㄴㅇ\n\n\nn\n\n\n\n\n     테스트',
-//   user: 123,
-//   createdAt: '2022-12-24T03:00:00.000Z',
-//   updatedAt: '2022-12-24T03:00:00.000Z',
-//   likes: [],
-// };
