e47c99fc21ec013d4b0013360a9effc9926162d7
younoah <kr_uno_10@naver.com>
2021-12-20 00:23:28 +0900

    refactor: 삭제버튼 스타일 수정

--- src/components/atoms/IconButton/Delete.tsx
+++ src/components/atoms/IconButton/Delete.tsx
@@ -17,38 +17,15 @@ const IconButton = styled.button`
   border: none;
   cursor: pointer;
 
-  @media ${Media.sm} {
-    border-radius: 12px;
-    width: 32px;
-    height: 32px;
-  }
-  @media ${Media.md} {
-    border-radius: 1rem;
-    width: 48px;
-    height: 48px;
-  }
-  @media ${Media.lg} {
-    border-radius: 1rem;
-    width: 48px;
-    height: 48px;
-  }
+  border-radius: 12px;
+  width: 40px;
+  height: 40px;
 `;
 
 const TrashIcon = styled(DeleteRoundedIcon)`
+  width: 24px;
+  height: 24px;
   color: ${Colors.point};
-
-  @media ${Media.sm} {
-    width: 1.5rem;
-    height: 1.5rem;
-  }
-  @media ${Media.md} {
-    width: 32px;
-    height: 32px;
-  }
-  @media ${Media.lg} {
-    width: 32px;
-    height: 32px;
-  }
 `;
 
 export default Delete;
