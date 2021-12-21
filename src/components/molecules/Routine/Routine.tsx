2b27bef2435ca2f3813b8b75673ee36a0c54dbe3
JunHyeongB <aowlr0513@gmail.com>
2021-12-21 16:18:45 +0900

    style: Routine box-shadow 스타일 추가

--- src/components/molecules/Routine/Routine.tsx
+++ src/components/molecules/Routine/Routine.tsx
@@ -140,6 +140,7 @@ const RoutineContainer = styled.div`
   text-align: center;
   cursor: pointer;
   position: relative;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
   @media ${Media.sm} {
     width: 8.75rem;
