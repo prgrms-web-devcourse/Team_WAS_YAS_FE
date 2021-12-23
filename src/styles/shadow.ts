0fd18c153ca8bd7c316ce53d66407ee85179e1e2
younoah <kr_uno_10@naver.com>
2021-12-23 18:27:24 +0900

    refactor: 버튼 그림자 스타일 변경

--- src/styles/shadow.ts
+++ src/styles/shadow.ts
@@ -1,5 +1,5 @@
 export const Shadow = Object.freeze({
-  button: '2px 2px 2px  rgba(0, 0, 0, 0.4)',
+  button: '2px 5px 6px  rgba(0, 0, 0, 0.4)',
   menu: '3px 2px 8px rgba(0, 0, 0, 0.7)',
   modal: '7px 14px 20px rgba(0, 0, 0, 1.2)',
   routine: '2px 5px 6px rgba(0, 0, 0, 1.2)',
