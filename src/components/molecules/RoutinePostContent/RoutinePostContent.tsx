96f3f1b36ab7a1dbcd6bf3b6fcb4bdf4201bc0cf
younoah <kr_uno_10@naver.com>
2022-01-21 18:06:09 +0900

    feat: 컨테트박스 펼치기 기능 적용

--- src/components/molecules/RoutinePostContent/RoutinePostContent.tsx
+++ src/components/molecules/RoutinePostContent/RoutinePostContent.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import { Colors, Media, FontSize } from '@/styles';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { SpreadToggle } from '../SpreadToggle';
 
 export interface RoutinePostContentProps extends React.ComponentProps<'div'> {
   content?: string;
@@ -10,23 +11,82 @@ const RoutinePostContent = ({
   content = '',
   ...props
 }: RoutinePostContentProps): JSX.Element => {
+  const ref = useRef<HTMLTextAreaElement>(null);
+  const [spreadable, setSpreadable] = useState<boolean>(false);
+  const [opened, setOpened] = useState<boolean>(false);
+  const scrollHeight = ref.current?.scrollHeight;
+
+  useEffect(() => {
+    if (!scrollHeight) return;
+    setSpreadable(scrollHeight > 29);
+  }, [setSpreadable, ref, scrollHeight]);
+
+  const handleClickSpreadToggle = () => {
+    setOpened((opened) => !opened);
+  };
+
   return (
     <Container {...props}>
-      <Text>{content}</Text>
+      <TextArea
+        opened={opened}
+        height={scrollHeight}
+        ref={ref}
+        readOnly
+        value={content}
+      >
+        {content}
+      </TextArea>
+      {spreadable && (
+        <SpreadToggleWrapper>
+          <SpreadToggle onClick={handleClickSpreadToggle} />
+        </SpreadToggleWrapper>
+      )}
     </Container>
   );
 };
 
 const Container = styled.div`
+  /* position: relative; */
   display: flex;
+  flex-direction: column;
   width: 100%;
-  height: 40px;
   margin-bottom: 1rem;
-  padding: 1rem;
+  padding: 1rem 1.5rem 0.5rem 1.5rem;
   border-radius: 0.5rem;
   background-color: ${Colors.backgroundButton}; ;
 `;
 
-const Text = styled.p``;
+const TextArea = styled.textarea<
+  React.ComponentProps<'textarea'> & {
+    opened: boolean;
+    height: number | undefined;
+  }
+>`
+  width: 100%;
+  height: ${({ opened, height }) => (opened ? `${height}px` : '1.8rem')};
+  overflow-y: hidden;
+  text-overflow: ellipsis;
+  background-color: transparent;
+  resize: none;
+  border: none;
+  outline: none;
+  color: ${Colors.textPrimary};
+
+  @media ${Media.sm} {
+    font-size: ${FontSize.small};
+  }
+  @media ${Media.md} {
+    font-size: ${FontSize.base};
+  }
+  @media ${Media.lg} {
+    font-size: ${FontSize.base};
+  }
+`;
+
+const SpreadToggleWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default RoutinePostContent;
