2c600e35f55d37784c7f417386cb358597aec44e
dasssseul <daseul8086@gmail.com>
2022-01-21 21:16:52 +0900

    feat : EmotionPicker 컴포넌트 기능 구현

--- src/components/organisms/RoutineReviewModal/EmotionPicker.tsx
+++ src/components/organisms/RoutineReviewModal/EmotionPicker.tsx
@@ -1,29 +1,44 @@
 import { EMOTION } from '@/constants';
 import { Media } from '@/styles';
 import styled from '@emotion/styled';
-import React from 'react';
+import React, { ChangeEvent, Fragment, useState } from 'react';
 
-export interface EmotionPickerProps extends React.ComponentProps<'input'> {
-  emotion: string;
+export interface EmotionPickerProps {
+  emotion?: string;
+  onChange: (emotion: string) => void;
 }
 
 const EmotionPicker = ({
   emotion,
+  onChange,
   ...props
 }: EmotionPickerProps): JSX.Element => {
+  const emotionList = Object.keys(EMOTION);
+  const [selectedEmotion, setSelectedEmotion] = useState<string>('1');
+  const handleEmotionChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSelectedEmotion(e.target.value);
+    onChange && onChange(e.target.value);
+  };
+
   return (
-    <>
-      <Input
-        type="radio"
-        id={emotion}
-        name="emotion"
-        value={emotion}
-        {...props}
-      />
-      <label htmlFor={emotion}>
-        <Image src={EMOTION[emotion]} alt="emotionImg" />
-      </label>
-    </>
+    <EmotionContainer>
+      {emotionList.map((emotion: string) => (
+        <Fragment key={emotion}>
+          <Input
+            type="radio"
+            id={emotion}
+            name="emotion"
+            value={emotion}
+            onChange={handleEmotionChange}
+            checked={selectedEmotion === emotion}
+            {...props}
+          />
+          <label htmlFor={emotion}>
+            <Image src={EMOTION[emotion]} alt="emotionImg" />
+          </label>
+        </Fragment>
+      ))}
+    </EmotionContainer>
   );
 };
 
@@ -38,17 +53,24 @@ const Input = styled.input`
 
 const Image = styled.img`
   @media ${Media.sm} {
-    width: 32px;
-    height: 32px;
+    width: 28px;
+    height: 28px;
+    margin: 0.6rem;
   }
   @media ${Media.md} {
     width: 48px;
     height: 48px;
+    margin: 0.8rem;
   }
   @media ${Media.lg} {
     width: 48px;
     height: 48px;
+    margin: 0.8rem;
   }
-  margin: 0.5rem;
   cursor: pointer;
+`;
+
+const EmotionContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
 `;
