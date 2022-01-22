7056a46953692ab405d4ffc06b18c255321657b2
younoah <kr_uno_10@naver.com>
2022-01-22 20:48:36 +0900

    feat: 연월 범위를 벗어나면 이동할 수 없도록 처리

--- src/components/organisms/Calendar/YearMonthPicker.tsx
+++ src/components/organisms/Calendar/YearMonthPicker.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import dayjs from 'dayjs';
+import isBetween from 'dayjs/plugin/isBetween';
 import styled from '@emotion/styled';
 // import { DatePicker, type DatePickerProps } from '@mui/lab';
 import { DatePicker } from '@mui/lab';
 import DateAdapter from '@mui/lab/AdapterDayjs';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import {
-  ArrowBackIosRounded as ArrowBackIcon,
-  ArrowForwardIosRounded as ArrowForwardIcon,
+  ArrowBackIosRounded as PrevIcon,
+  ArrowForwardIosRounded as NextIcon,
 } from '@mui/icons-material';
 import { IconButton, TextField } from '@mui/material';
+import { MIN_DATE } from './constants';
 
 // export type YearMonthPickerProps = DatePickerProps;
+dayjs.extend(isBetween);
 export interface YearMonthPickerProps extends React.ComponentProps<'div'> {
   onChangeYearMonth?: (date: Date) => void;
 }
@@ -20,26 +23,41 @@ const YearMonthPicker = ({
   onChangeYearMonth,
   ...props
 }: YearMonthPickerProps): JSX.Element => {
-  const [value, setValue] = React.useState<Date>(new Date());
+  const [date, setDate] = React.useState<dayjs.Dayjs>(dayjs());
 
   const handleChange = (date: any) => {
-    setValue(date);
+    setDate(date);
     onChangeYearMonth && onChangeYearMonth(date);
+  };
+
+  const handleClickPrevIcon = () => {
+    setDate((date) => date.add(-1, 'month'));
+  };
+
+  const handleClickNextIcon = () => {
+    setDate((date) => date.add(1, 'month'));
   };
 
   return (
     <Container {...props}>
-      <IconButton>
-        <ArrowBackIcon />
+      <IconButton
+        onClick={handleClickPrevIcon}
+        disabled={
+          date.get('year') === dayjs(MIN_DATE).get('year') &&
+          date.get('month') <= dayjs(MIN_DATE).get('month')
+        }
+      >
+        <PrevIcon />
       </IconButton>
       <LocalizationProvider dateAdapter={DateAdapter}>
         <Picker
           views={['year', 'month']}
           // label="Year and Month"
+          // readOnly
           inputFormat="YYYY년 MM월"
-          minDate={dayjs('2021-01-01')}
+          minDate={dayjs(MIN_DATE)}
           maxDate={dayjs()}
-          value={value}
+          value={date}
           onChange={handleChange}
           renderInput={(params) => (
             <YearMonthTextField
@@ -52,8 +70,14 @@ const YearMonthPicker = ({
           )}
         />
       </LocalizationProvider>
-      <IconButton>
-        <ArrowForwardIcon />
+      <IconButton
+        onClick={handleClickNextIcon}
+        disabled={
+          date.get('year') === dayjs().get('year') &&
+          date.get('month') >= dayjs().get('month')
+        }
+      >
+        <NextIcon />
       </IconButton>
     </Container>
   );
