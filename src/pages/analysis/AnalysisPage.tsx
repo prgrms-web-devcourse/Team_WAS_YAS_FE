630ab654de48f293b88776c4c421c5db860cda9e
younoah <kr_uno_10@naver.com>
2022-01-31 14:54:26 +0900

    feat: 분석페이 초기화 설정

--- src/pages/analysis/AnalysisPage.tsx
+++ src/pages/analysis/AnalysisPage.tsx
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Container, Calendar } from '@/components';
 import styled from '@emotion/styled';
 import { Colors, FontSize, FontWeight, Media } from '@/styles';
 import { routineStatusApi } from '@/apis';
+import dayjs from 'dayjs';
+
+type highlightDatesType = {
+  [key: string]: number;
+};
+
+type routineStatusType = {
+  dateTime: string;
+};
+
+const parseHighlightDates = (
+  routineStatusData: routineStatusType[],
+): highlightDatesType => {
+  const highlightDates: highlightDatesType = {};
+  routineStatusData.forEach((routineStatus: routineStatusType) => {
+    const date = dayjs(routineStatus.dateTime).format('YYYY-MM-DD');
+    highlightDates[date]
+      ? (highlightDates[date] += 1)
+      : (highlightDates[date] = 1);
+  });
+  return highlightDates;
+};
 
 const AnalysisPage = (): JSX.Element => {
+  const [highlightDates, setHighlightDates] = useState<highlightDatesType>({});
+
+  const handleClickDate = (date: dayjs.Dayjs) => {
+    console.log('handleClickDate');
+    // getRoutineStatusByDate(date);
+  };
+
+  const getRoutineStatusByDate = useCallback(async (date: string) => {
+    try {
+      const res = await routineStatusApi.getRoutineStatusByDate(date);
+      const routineStatus = res.data.data;
+      return routineStatus;
+    } catch (error) {
+      return [];
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log('useEffect');
+
+    const init = async () => {
+      const today = dayjs().format('YYYY-MM');
+      const routineStatusData = await getRoutineStatusByDate(today);
+      const highlightDates = parseHighlightDates(routineStatusData);
+      setHighlightDates(highlightDates);
+      console.log(routineStatusData, highlightDates);
+    };
+
+    init();
+  }, []);
+
   return (
     <Container navBar>
-      <Calendar />
+      <Calendar highlightDates={highlightDates} onClickDate={handleClickDate} />
     </Container>
   );
 };
