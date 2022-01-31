7c8643f7597ac88b27c66639a43a3bbd8db73e00
younoah <kr_uno_10@naver.com>
2022-01-31 14:25:21 +0900

    refactor: routineStatus api 오타 수정

--- src/pages/analysis/AnalysisPage.tsx
+++ src/pages/analysis/AnalysisPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Calendar } from '@/components';
 import styled from '@emotion/styled';
 import { Colors, FontSize, FontWeight, Media } from '@/styles';
+import { routineStatusApi } from '@/apis';
 
 const AnalysisPage = (): JSX.Element => {
   return (
